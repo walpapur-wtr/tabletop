39ab363097c7965333f67ca99d8c8530e41d8d70
maksym-walp <maksymbohach@gmail.com>
2025-02-26 16:57:26 +0200

    Created some gane-system configuration files. Edited form. Now characters creation form have different fields depending on config file

--- src/Character/CharacterGrid-component.jsx
+++ src/Character/CharacterGrid-component.jsx
@@ -1,4 +1,3 @@
-// CharacterGrid.js
 import React, { useState, useEffect } from "react";
 import { CharacterCard } from "./CharacterCard-component";
 import { CharacterForm } from "./CharacterForm-component";
@@ -24,7 +23,13 @@ export const CharacterGrid = () => {
       body: JSON.stringify(newCharacter),
     })
       .then((res) => res.json())
-      .then((data) => setCharacters((prev) => [...prev, data]))
+      .then((data) => {
+        if (data.character) {
+          setCharacters((prev) => [...prev, data.character]);
+        } else {
+          console.error("Error creating character:", data.error);
+        }
+      })
       .catch((err) => console.error(err));
 
     setIsFormVisible(false);
