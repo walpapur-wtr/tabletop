7b40c6252ba213433635daf57a8eb9363b55668a
maksym-walp <maksymbohach@gmail.com>
2025-04-04 17:16:20 +0300

    Fixed dice-roller component stules and adaptations

--- src/components/Footer-component.jsx
+++ src/components/Footer-component.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function Footer() {
     return (
         <footer className="footer">
-            <p>Walpapur Studio</p>
+            <p>Walpapur Studio. Beta 0.3</p>
+            <p>Development plan</p>
+            <ol>
+                <li>Beta 0.3 - 16.04</li>
+                <li>Beta 0.4 - 30.04</li>
+                <li>Beta 0.5 - 20.05</li>
+            </ol>
         </footer>
     );
 }
